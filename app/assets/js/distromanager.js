d2fa10cc009a802f5d5c920b9b0ce0af6884da5c
Squxfn <73950491+Squxfn@users.noreply.github.com>
2026-03-30 08:10:06 +0900

    Update REMOTE_DISTRO_URL to new endpoint

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/RMkuran/ModPacks/main/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://modpacks.rmkuran.workers.dev/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
